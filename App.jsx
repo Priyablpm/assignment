497f6febdb752d979352211a2aeac50d8f26179c
Priyablpm <120570234+Priyablpm@users.noreply.github.com>
2023-03-06 09:47:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './component/Nav'
+import "./global.css"
+
+const App = () => {
+  return (
+    <>
+    <Nav/>   
+    </>
+    
+  )
+}
+
+export default App
---
+++ App1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav1 from "./twitter/Nav1";
+import "./global1.css"
+const App1 = () =>{
+    return(
+        <>
+<Nav1/>
+</>
+    )
+}
+export default App1
